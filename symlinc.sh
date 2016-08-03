ba0dba08ee409534812872594f9826c496c7b2e9
Kaido Iwamoto <kaido@Kaidos-MacBook-Pro.local>
2016-08-03 10:41:16 +0900

    Modify and rename symlinc.sh

--- symlinc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-path=`pwd`
-ln -s ${path}/.vimrc ~/.vimrc
-ln -s ${path}/.gvimrc ~/.gvimrc
-ln -s ${path}/.vim   ~/.vim
-ln -s ${path}/.zshrc ~/.zshrc
-ln -s ${path}/.gitconfig ~/.gitconfig
-ln -s ${path}/.gitignore-global ~/.gitignore
---
+++ symlink.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+path=`pwd`
+ln -s $path/.vimrc $HOME/.vimrc
+ln -s $path/.gvimrc $HOME/.gvimrc
+ln -s $path/.vim $HOME/.vim
+ln -s $path/.zshrc $HOME/.zshrc
+ln -s $path/.gitconfig $HOME/.gitconfig
+ln -s $path/.gitignore-global $HOME/.gitignore
